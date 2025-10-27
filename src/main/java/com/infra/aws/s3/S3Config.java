acdb2b5266bf19248e5a1e8630622f1aba5cb9f8
Yoepee <kdy2975@naver.com>
2025-10-27 16:02:17 +0900

    계정 인증정보 env

--- src/main/java/com/infra/aws/s3/S3Config.java
+++ src/main/java/com/infra/aws/s3/S3Config.java
@@ -1,5 +1,6 @@
 package com.infra.aws.s3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -7,6 +8,12 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+    @Value("${custom.cloud.aws.credentials.access-key}")
+    private String s3AccessKey;
+
+    @Value("${custom.cloud.aws.credentials.secret-key}")
+    private String s3SecretKey;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
