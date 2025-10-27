c1409056e45b5e662cb852fb75d2f99969954749
Yoepee <kdy2975@naver.com>
2025-10-27 15:00:56 +0900

    s3 연결

--- src/main/java/com/infra/TerraformSpring251027Application.java
+++ src/main/java/com/infra/TerraformSpring251027Application.java
@@ -2,7 +2,9 @@ package com.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TerraformSpring251027Application {
 
