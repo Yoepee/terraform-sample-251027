d2d5f074d054c78ebcf8cf92daa302fe6449c7e9
Yoepee <kdy2975@naver.com>
2025-10-27 15:00:42 +0900

    초기세팅

---
+++ src/main/java/com/infra/TerraformSpring251027Application.java
@@ -0,0 +1,13 @@
+package com.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformSpring251027Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraformSpring251027Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/infra/TerraformSpring251027ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformSpring251027ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
